ae1cf98b0cf67663c56ecb2b67d50aee2c702e21
Groff <guilherme.pgb@hotmail.com>
2021-10-03 16:32:00 -0300

    Adição de log
    Signed-off-by: Groff <guilherme.pgb@hotmail.com>

--- src/main/java/com/example/securingweb/controler/impl/RelatorioController.java
+++ src/main/java/com/example/securingweb/controler/impl/RelatorioController.java
@@ -1,5 +1,7 @@
 package com.example.securingweb.controler.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +13,13 @@ import com.example.securingweb.model.service.mail.EmailHelper;
 @Controller
 public class RelatorioController {
 
+	private static Logger logger = LoggerFactory.getLogger(RelatorioController.class);
+
 	@GetMapping("/relatorioMensal")
 	public boolean enviarRelatorioMensal(@RequestParam String username, @RequestParam int mes, @RequestParam int meta,
 			@RequestParam String email) {
+
+		logger.info("Inicio geracao relatorio mensal para o usuario: {}", username);
 
 		email = email.replace("%40", "@");
 
@@ -23,6 +29,8 @@ public class RelatorioController {
 
 		EmailHelper emailHelper = new EmailHelper(email, corpoEmail, relatorio);
 		emailHelper.criar();
+
+		logger.info("Fim geracao relatorio mensal para o usuario: {}", username);
 
 		return true;
 	}
