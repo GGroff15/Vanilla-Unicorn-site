2efc712d5f3a71f680f4c1f7ac0398fa3cb9e4cc
guilhermebenedeti <guilhermebenedeti@brq.com>
2022-02-14 08:23:48 -0300

    Adicionada entidade lista de userDto
    Signed-off-by: guilhermebenedeti <guilhermebenedeti@brq.com>

---
+++ src/main/java/br/com/vanilla/site/entity/Usuarios.java
@@ -0,0 +1,24 @@
+package br.com.vanilla.site.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuarios implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8078987631650244547L;
+
+	private List<UserDTO> usuarioDTOs = new ArrayList<>();
+
+	public List<UserDTO> getUsuarioDTOs() {
+		return usuarioDTOs;
+	}
+
+	public void setUsuarioDTOs(List<UserDTO> usuarioDTOs) {
+		this.usuarioDTOs = usuarioDTOs;
+	}
+
+}
